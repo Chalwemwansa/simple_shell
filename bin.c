427c4bc148b59e65a478734c976175b0992dd249
chalwemwansa <chalwemwansa23@gmail.com>
2023-08-23 22:41:02 +0200

    commit by chalwe

---
+++ bin.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * bin - checks if /bin/ is already included
+ *
+ * tocken: the tocken to be checked
+ * Return: 0 if /bin/ is not included
+ */
+int bin(char *tocken)
+{
+	int i = 0;
+	char *bin = "/bin/";
+
+	while (i <= 4)
+	{
+		if (bin[i] != tocken[i])
+			return (0);
+		i++;
+	}
+	return (1);
+}
